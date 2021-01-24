0332065fdfc503dd4e67f64f69cec62529ba5a8b
denniszicodes <denniszicodes@users.noreply.github.com>
2021-01-24 14:58:54 +0100

    add cors middleware

--- server/app.js
+++ server/app.js
@@ -22,6 +22,19 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.get("/login", (req, res, next) => {
   const state = uuid();
   const scopes = "user-read-private user-read-email";
