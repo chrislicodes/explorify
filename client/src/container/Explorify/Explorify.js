1b5f2340faf7d7945116bdfc270d120064e41755
denniszicodes <denniszicodes@users.noreply.github.com>
2021-02-20 21:48:36 +0100

    updated explorify container

--- client/src/container/Explorify/Explorify.js
+++ client/src/container/Explorify/Explorify.js
@@ -1,37 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { getRecentlyPlayed } from "../../api/index";
-import { Switch, Route, Link } from "react-router-dom";
-
-import SongContainer from "../../components/SongContainer/SongContainer";
-
-import Home from "../Home/Home";
-import Analyze from "../Analyze/Analyze";
-
-import classes from "./Explorify.module.css";
+import React from "react";
 
 const Explorify = ({ children }) => {
-  console.log(children);
-  // const [recentlyPlayedSongs, setRecentlyPlayedSongs] = useState(null);
-
-  // //LIFECYCLE
-  // useEffect(() => {
-  //   getRecentlyPlayed().then((res) => setRecentlyPlayedSongs(res.data));
-  // }, []);
-
   return (
     <div>
       <div>{children}</div>
     </div>
-    // <Switch>
-    //   <Route component={Home} />
-    //   <Route exact to="/analyze" component={Analyze} />
-    // </Switch>
-
-    // <div className={classes.Explorify}>
-    //   {recentlyPlayedSongs ? (
-    //     <SongContainer tracks={recentlyPlayedSongs.items} />
-    //   ) : null}
-    // </div>
   );
 };
 
