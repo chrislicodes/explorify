d5f6caefae815c3588635b98aa126e1132fde87f
denniszicodes <denniszicodes@users.noreply.github.com>
2021-02-23 13:33:53 +0100

    design loginscreen
    - designed loginscreen -> reference figma

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -1,10 +1,43 @@
 import React from "react";
 
+import classes from "./Login.module.css";
+import SpotifyIcon from "../../layouts/Explorify/SideNav/AppTitle/SpotifyIcon/SpotifyIcon";
+
 const Login = function () {
   return (
-    <button>
-      <a href="http://localhost:8080/login">Login!</a>
-    </button>
+    <div className={classes.LoginContainer}>
+      <div className={classes.LoginBackdrop}></div>
+      <div className={classes.LoginLogo}>
+        <SpotifyIcon />
+      </div>
+      <main className={classes.LoginContent}>
+        <div className={classes.LoginWrapper}>
+          <div className={classes.LoginHeadline}>
+            <p>
+              Welcome to
+              <span>
+                <b> EXPLORIFY</b>.
+              </span>
+            </p>
+          </div>
+          <div className={classes.LoginSubheadline}>
+            <p>Explore your favorite songs and artists on Spotify</p>
+          </div>
+          <div className={classes.LoginBtnContainer}>
+            <a className={classes.LoginBtn} href="http://localhost:8080/login">
+              LOGIN TO SPOTIFY
+            </a>
+            <a className={classes.LoginLearnMoreBtn} href="/">
+              LEARN MORE
+            </a>
+          </div>
+        </div>
+      </main>
+      <div className={classes.AppVersion}>v.0.0.1</div>
+      <div className={classes.AppNote}>
+        This application is not an official product of Spotify, but uses Spotify's API.
+      </div>
+    </div>
   );
 };
 
