deba8e4728408e7670af80f61ed74598b089b995
Christoph Lindstädt <45950730+DataLind@users.noreply.github.com>
2021-02-23 23:05:39 +0100

    initial setup

--- client/src/layouts/Explorify/SideNav/SideNav.js
+++ client/src/layouts/Explorify/SideNav/SideNav.js
@@ -3,12 +3,21 @@ import classes from "./SideNav.module.css";
 import NavItem from "./NavItem/NavItem";
 import AppTitle from "./AppTitle/AppTitle";
 
+const links = [
+  { target: "/", icon: "icon-home" },
+  { target: "/analyze", icon: "icon-search" },
+  { target: "/explore", icon: "icon-compass" },
+  { target: "/about", icon: "icon-notification" },
+];
+
 const Header = () => {
   return (
     <nav className={classes.SideNav}>
       <AppTitle />
-      <ul>
-        <NavItem target="Analyze" />
+      <ul className={classes.NavLinks}>
+        {links.map((link) => (
+          <NavItem target={link.target} icon={link.icon} />
+        ))}
       </ul>
     </nav>
   );
